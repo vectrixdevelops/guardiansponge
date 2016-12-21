998842eebed09a9885cb311da646f2d60a2b87fc
Connor Hartley <vectrixu@gmail.com>
2016-12-21 17:09:39 +1300

    Add data stubs and violation stubs

--- src/main/java/io/github/connorhartley/guardian/Guardian.java
+++ src/main/java/io/github/connorhartley/guardian/Guardian.java
@@ -28,11 +28,13 @@ import com.me4502.modularframework.ModuleController;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.game.state.GameStartingServerEvent;
 import org.spongepowered.api.event.game.state.GameStoppingEvent;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.plugin.PluginContainer;
 
