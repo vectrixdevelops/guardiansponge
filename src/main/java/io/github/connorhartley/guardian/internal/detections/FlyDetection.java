ab914e86aa376b9b11a084beb4c6a7f6c4f97a23
connorhartley <vectrixu@gmail.com>
2017-05-24 11:11:04 +1200

    Bump detection versions

--- src/main/java/io/github/connorhartley/guardian/internal/detections/FlyDetection.java
+++ src/main/java/io/github/connorhartley/guardian/internal/detections/FlyDetection.java
@@ -67,7 +67,7 @@ import java.util.Optional;
         id = "fly",
         name = "Fly Detection",
         authors = { "Connor Hartley (vectrix)" },
-        version = "0.0.18",
+        version = "0.0.19",
         onEnable = "onConstruction",
         onDisable = "onDeconstruction"
 )
